ba5eb3a2a1f178d06189452fca21abcca10fbc60
jennvance <vancejenniferm@gmail.com>
2014-09-03 10:35:47 -0600

    first

---
+++ public/javascripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+	// $(".nav1 li > a").on("click", function () {
+	// 	$(".nav1 li > a").removeClass("active");
+	// 	$(this).addClass("active");
+	// });
+	
+	// $('#myTab a').click(function (e) {
+ //  		e.preventDefault()
+ //  		$(this).tab('show')
+	// })
+
+
+});	
